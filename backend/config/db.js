52cb83056479ad992525302065c1799fd30f6846
Ojaschikara <ojaschikara@email.com>
2024-09-22 17:16:27 +0530

    added

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const  connection = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected`);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+   
+  }
+};
+
+export default connection
---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 8080;
+
+
+
+
+app.listen(PORT,async()=>{
+    try {
+       await connection;
+       console.log(`Server is running on PORT: ${PORT} and DB is also connected`) 
+    } catch (error) {
+        console.log(`Error occured during server connection${error}`)
+        
+    }
+})
